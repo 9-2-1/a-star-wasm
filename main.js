f59f2f3a6777c2591506f3c9429044aec4dd94bb
six-6 <1195132772@qq.com>
2022-04-03 12:08:15 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+let fs = require("fs");
+(async () => {
+	let wabt = await (require("wabt")());
+	// let module = wabt.readWasm(fs.readFileSync("main.wasm"), { readDebugNames: true});
+	let module = wabt.parseWat("main.wast",fs.readFileSync("main.wast").toString());
+	module.validate();
+
+	let bin = module.toBinary({}).buffer;
+	module.destroy();
+	fs.writeFileSync("main.wasm",bin);
+
+	let wmodule = await WebAssembly.compile(bin);
+	let instance = await WebAssembly.instantiate(wmodule);
+
+	// test everything here
+	console.log(instance.exports.a(32n));
+
+	// fs.writeFileSync("main.wat",module.toText({foldExprs: true, inlineExport: true}));
+	// module.destroy();
+}) () ;
---
+++ main.wast
@@ -0,0 +1,8 @@
+(module
+	(memory (;min;) 0 (;max;) 104857600(;=100MB;))
+	(func $a (export "a")
+				(param $x i64)
+				(result i64)
+				(local.get $x)
+				)
+	)
